136b5d694db735c1c6d9bd4157aaf19279f65de2
AlistairHudson <alistairhudson64@gmail.com>
2023-06-29 14:09:54 +0300

    Removed passing strategies

--- Table Top AR/Assets/Scripts/Character/Abilities/AbilityData.cs
+++ Table Top AR/Assets/Scripts/Character/Abilities/AbilityData.cs
@@ -9,15 +9,11 @@ namespace TableTopAR.Character.Abilities
         private GameObject _user = null;
         private Vector3 _targetedPoint = Vector3.zero;
         private IEnumerable<GameObject> _targets = null;
-        private FilterStrategy[] _filterStrategies;
-        private EffectStrategy[] _effectStrategies;
         private List<GameObject> _chainedTargets = new List<GameObject>();
         private int _chained = 0;
         private bool _isUserCaster = false;
 
         public GameObject User { get => _user; }
-        public FilterStrategy[] FilterStrategies { get => _filterStrategies; }
-        public EffectStrategy[] EffectStrategies { get => _effectStrategies; }
         public bool IsUserCaster { get => _isUserCaster; }
 
         public Vector3 TargetedPoint { get => _targetedPoint; set => _targetedPoint = value; }
@@ -25,11 +21,9 @@ namespace TableTopAR.Character.Abilities
         public List<GameObject> ChainedTargets { get => _chainedTargets; set => _chainedTargets = value; }
         public int Chained { get => _chained; set => _chained = value; }
 
-        public AbilityData(GameObject user, FilterStrategy[] filterStrategies, EffectStrategy[] effectStrategies, bool isCaster = false)
+        public AbilityData(GameObject user, bool isCaster = false)
         {
             _user = user;
-            _filterStrategies = filterStrategies;
-            _effectStrategies = effectStrategies;
             _isUserCaster = isCaster;
         }
     }
